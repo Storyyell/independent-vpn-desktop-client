cdf3049e08679354feebf02ea2404ea915b23649
anandanmb01 <anandanmb01@gmail.com>
2024-03-10 17:38:30 +0530

    VpnStatusMainContext added

---
+++ src/renderer/src/context/VpnStatusMainContext.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { createContext, useState } from "react";
+
+const VpnStatusMainContext = createContext({});
+
+function VpnStatusMainProvider({ children }) {
+    const [vpnStatusMain, setVpnStatusMain] = useState('disconnected');
+
+    return (
+        <VpnStatusMainContext.Provider value={{ vpnStatusMain, setVpnStatusMain }}>
+            {children}
+        </VpnStatusMainContext.Provider>
+    );
+}
+
+export { VpnStatusMainContext, VpnStatusMainProvider };
--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -4,11 +4,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ServerListProvider } from './context/ServerListContext'
+import { VpnStatusMainProvider } from './context/VpnStatusMainContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <ServerListProvider>
-    <App />
+    <ServerListProvider>
+      <VpnStatusMainProvider>
+        <App />
+      </VpnStatusMainProvider>
     </ServerListProvider>
   </React.StrictMode>
 )
