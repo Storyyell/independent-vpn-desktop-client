2fe33ac8211a23c1e874929da7cec9c95c04cdeb
anandanmb01 <anandanmb01@gmail.com>
2024-03-10 22:15:31 +0530

    inital empty country list issue fixed

--- src/renderer/src/components/LocationSelection/Countries.jsx
+++ src/renderer/src/components/LocationSelection/Countries.jsx
@@ -9,15 +9,26 @@ const Countries = (props) => {
 
     const [countries, setCountries] = React.useState(localStorage.getItem("country_list") ? localStorage.getItem("country_list") : [])
     useEffect(() => {
-        if(localStorage.getItem("device_token")){
-        window.api.getCountries(localStorage.getItem("device_token"))
-            .then((res) => {
-                localStorage.setItem("country_list", res.data)
-                setCountries(res.data)
-            })
-            .catch((e) => {
-                console.log(e)
-            })
+        if (localStorage.getItem("device_token")) {
+            window.api.getCountries(localStorage.getItem("device_token"))
+                .then((res) => {
+                    localStorage.setItem("country_list", res.data)
+                    setCountries(res.data)
+                })
+                .catch((e) => {
+                    console.log(e)
+                })
+        } else { //Todo fix below part by context
+            setTimeout(() => {
+                window.api.getCountries(localStorage.getItem("device_token"))
+                    .then((res) => {
+                        localStorage.setItem("country_list", res.data)
+                        setCountries(res.data)
+                    })
+                    .catch((e) => {
+                        console.log(e)
+                    })
+            }, 3000)
         }
 
     }, [])
