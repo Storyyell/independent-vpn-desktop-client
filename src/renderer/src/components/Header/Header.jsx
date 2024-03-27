6e057ea809a5fed9e8d905b40e4cba0e1f56d7cc
anandanmb01 <anandanmb01@gmail.com>
2024-03-27 17:37:29 +0530

    added upgrade to premium button

--- src/renderer/src/components/Header/Header.jsx
+++ src/renderer/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { IconButton, Stack } from '@mui/material'
+import { Button, IconButton, Stack, Typography } from '@mui/material'
 import React from 'react'
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
@@ -12,13 +12,26 @@ const Header = () => {
   }
 
   return (
-    <Stack direction={'row'} width={'100%'} style={{ justifyContent: 'space-between' }}>
+    <Stack direction={'row'} width={'100%'} style={{ justifyContent: 'space-between', alignItems: 'center' }}>
       <IconButton sx={{ p: 1, m: 1 }} onClick={handleMenuClick}>
         {menuClick ? < CloseIcon /> : <MenuIcon />}
       </IconButton>
-      <IconButton>
-        <MenuIcon />
-      </IconButton>
+
+      <Button
+        variant="contained"
+        size='small'
+        sx={{
+          height: '30px',
+          borderRadius: "15px",
+          mx: 2,
+          my: 1,
+          p: 1,
+        }}>
+        <Typography variant="overline" sx={{ m: 1, fontSize: '9px' }}>
+          Upgrade to Premium
+        </Typography>
+
+      </Button>
     </Stack>
   )
 }
