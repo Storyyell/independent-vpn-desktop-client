f61d72cc0828c4873f6991f125d20a8ca560e81e
anandanmb01 <anandanmb01@gmail.com>
2024-04-02 00:06:52 +0530

    updated header

--- src/renderer/src/components/Header/Header.jsx
+++ src/renderer/src/components/Header/Header.jsx
@@ -17,46 +17,42 @@ const Header = () => {
     setMenuClick(!menuClick)
   }
 
-  const mentIconStyle = {
-    width: '32px',
-    height: '32px'
-  }
-
 
   return (
-    <Stack direction={'row'} width={'100%'} style={{ justifyContent: 'space-between', alignItems: 'center', padding: '16px 24px' }}>
+    <Box width='100%'>
+      <Stack direction={'row'} style={{ justifyContent: 'space-between', alignItems: 'center', margin: '10px 10px' }}>
 
-      <IconButton onClick={handleMenuClick}>
-        {menuClick ? < CloseIcon style={mentIconStyle} /> : <MenuIcon style={mentIconStyle} />}
-      </IconButton>
+        <IconButton onClick={handleMenuClick}>
+          {menuClick ? < CloseIcon /> : <MenuIcon />}
+        </IconButton>
 
-      {/* side drawer */}
+        {/* side drawer */}
 
-      <Drawer open={menuClick} onClose={() => { setMenuClick(false) }} >
-        <Box sx={{ width: '90vw', p: 4, height: '100vh' }} role="presentation"
-          style={{
-            background: 'linear-gradient(180deg, #1E1A1B 0%, #171414 100%)'
+        <Drawer open={menuClick} onClose={() => { setMenuClick(false) }} >
+          <Box sx={{ width: '90vw', p: 4, height: '100vh' }} role="presentation"
+            style={{
+              background: 'linear-gradient(180deg, #1E1A1B 0%, #171414 100%)'
+            }}>
+            <Settings onClick={() => { setMenuClick(false) }} />
+          </Box>
+        </Drawer>
+
+        {/* <Button
+          variant="outlined"
+          size='small'
+          color='error'
+          sx={{
+            color: 'white',
+            borderRadius: '20px',
           }}>
-          <Settings onClick={() => { setMenuClick(false) }} />
-        </Box>
-      </Drawer>
+          <Typography variant="overline" sx={{ fontSize: '12px', color: 'white' }}>
+            Upgrade to Premium
+          </Typography>
+        </Button> */}
 
-      <Button
-        variant="contained"
-        size='small'
-        sx={{
-          height: '32px',
-          borderRadius: '16px',
-          background: '#CC2229',
-          backdropFilter: 'blur(2px)'
+      </Stack>
+    </Box>
 
-        }}>
-        <Typography variant="overline" sx={{ fontSize: '12px', color: 'white' }}>
-          Upgrade to Premium
-        </Typography>
-      </Button>
-
-    </Stack>
   )
 }
 
