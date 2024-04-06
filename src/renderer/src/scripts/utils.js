07f275fcc1d0a16ab1a3a0eaee567e6c93fd3933
anandanmb01 <anandanmb01@gmail.com>
2024-04-07 03:59:07 +0530

    _

--- src/renderer/src/scripts/utils.js
+++ src/renderer/src/scripts/utils.js
@@ -8,9 +8,12 @@ async function refreshCountryList(deviceToken, serverList, setServerList) {
   if (deviceToken && (!lastRefreshTimestamp || (now - lastRefreshTimestamp) >= dataValidityPeroid)) {
     try {
       const res = await window.api.getCountries(deviceToken);
-      setServerList((d) => {
-        return { ...d, countries: { data: res.data, timestamp: new Date() } }
-      });
+      const updatedServerList = {
+        ...serverList,
+        countries: { data: res.data, timestamp: new Date() }
+      };
+      setServerList(updatedServerList);
+      return updatedServerList;
     } catch (e) {
       console.log(e);
     }
@@ -26,14 +29,15 @@ async function refreshCityList(countryId, deviceToken, serverList, setServerList
   if ((!lastRefreshTimestamp || now - lastRefreshTimestamp > dataValidityPeroid) && countryId && deviceToken) {
     try {
       const res = await window.api.getCities(deviceToken, countryId);
-      setServerList((d) => {
-        return {
-          ...d, cities: {
-            ...d.cities,
-            [countryId]: { data: res.data, timestamp: new Date() }
-          }
+      const updatedServerList = {
+        ...serverList,
+        cities: {
+          ...serverList.cities,
+          [countryId]: { data: res.data, timestamp: new Date() }
         }
-      });
+      };
+      setServerList(updatedServerList);
+      return updatedServerList;
     } catch (e) {
       console.log(e);
     }
@@ -51,14 +55,15 @@ async function refreshServerList(countryId, cityId, setServerList, serverList, d
   if ((!lastRefreshTimestamp || now - lastRefreshTimestamp > dataValidityPeroid) && deviceToken && countryId && cityId) {
     try {
       const res = await window.api.getServers(deviceToken, countryId, cityId);
-      setServerList((d) => {
-        return {
-          ...d, servers: {
-            ...d.servers,
-            [`${countryId}-${cityId}`]: { data: res.data, timestamp: new Date() }
-          }
+      const updatedServerList = {
+        ...serverList,
+        servers: {
+          ...serverList.servers,
+          [`${countryId}-${cityId}`]: { data: res.data, timestamp: new Date() }
         }
-      });
+      };
+      setServerList(updatedServerList);
+      return updatedServerList;
     } catch (e) {
       console.log(e);
     }
