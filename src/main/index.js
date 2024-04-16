3a0355b2158f9a042a3354995c3da1f0d5b14e08
anandanmb01 <anandanmb01@gmail.com>
2024-04-16 17:29:21 +0530

    ipcHandler code splitted

--- src/main/index.js
+++ src/main/index.js
@@ -4,15 +4,12 @@ import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
 import icon_ from '../../resources/icon.ico?asset'
-import { vpnConnet, vpnDisconnect } from './system/vpnBase.js'
 import os from 'os'
 import { v4 as uuidv4 } from 'uuid';
 import fs from 'fs'
 import path from 'path'
 import { vpnObj } from './system/vpnBase.js'
-import { registerDevice, pullCountryList, pullCityList, pullServerList, pullServerConf, getIp, showNotification, getVpnMetric } from './system/ipcs.js'
-import { dnsList } from './system/dns/dnsList.js'
-import { adapterSpeed } from './system/stats/adapter.js'
+import { registerIpcHandlers } from './ipcHandlers/handlers.js';
 
 let sessionTempDir = {
   path: '',
@@ -67,90 +64,7 @@ function createWindow() {
     return { action: 'deny' }
   })
 
-  ipcMain.handle('triggerConnection', async (event, serverParms) => {
-    console.log('vpn connection trigger on main process')
-    return await vpnConnet(serverParms);
-  })
-
-  ipcMain.handle('triggerDisconnection', async (event) => {
-    console.log('vpn disconnection trigger on main process')
-    return await vpnDisconnect()
-  })
-
-  ipcMain.handle('vpnConnStatus', (event, serverObj) => {
-    return global.vpnConnStatus
-  })
-
-  ipcMain.handle('registerDevice', async () => {
-    console.log('device registration triggered on main process')
-    return registerDevice()
-  })
-
-  ipcMain.handle('getCountries', async (event, args) => {
-    return pullCountryList(args)
-  })
-
-  ipcMain.handle('getCities', async (event, device_token, countryCode) => {
-    return pullCityList(device_token, countryCode)
-  })
-
-  ipcMain.handle('getServers', async (event, device_token, countryCode, cityCode) => {
-    return pullServerList(device_token, countryCode, cityCode)
-  })
-
-  ipcMain.handle('getServerConf', async (event, device_token, countryCode, cityCode, serverId) => {
-    return pullServerConf(device_token, countryCode, cityCode, serverId)
-  })
-
-  ipcMain.handle('sysOpen', async (event, ...url) => {
-    shell.openExternal(url[0], { activate: true });
-  })
-
-  ipcMain.handle('getIp', async (event, device_token) => {
-    return getIp(device_token);
-  })
-
-  ipcMain.handle('appVersion', async (event) => {
-    let appVersion = app.getVersion();
-    return appVersion;
-  })
-
-  ipcMain.handle('openMailClient', async (event, mailObj) => {
-    return new Promise((resolve, reject) => {
-      const mailtoLink = `mailto:${mailObj.to}?subject=${mailObj.subject}&body=${mailObj.body}`;
-      shell.openExternal(mailtoLink, { activate: true }, (error) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve();
-        }
-      });
-    });
-  })
-
-  ipcMain.handle('pushNotification', async (event, title, body) => {
-    if (title && body) {
-      showNotification(title, body);
-    }
-
-  })
-
-  ipcMain.handle('vpnMetric', async (event) => {
-    return getVpnMetric();
-  })
-
-  ipcMain.handle('getDnsList', async (event) => {
-
-    return {
-      dnsList: dnsList,
-      selectedDns: vpnObj.dnsIndex
-    };
-
-  })
-  ipcMain.handle('setDns', async (event, dnsId) => { vpnObj.dnsIndex = dnsId || 0; })
-
-  ipcMain.handle('adapterSpeed', async (event) => { return adapterSpeed(global.adapterName); });
-
+  registerIpcHandlers(ipcMain);
 
   // HMR for renderer base on electron-vite cli.
   // Load the remote URL for development or the local html file for production.
---
+++ src/main/ipcHandlers/handlers.js
@@ -0,0 +1,104 @@
+import { app, shell, } from 'electron'
+import { vpnConnet, vpnDisconnect } from '../system/vpnBase.js'
+import { vpnObj } from '../system/vpnBase.js'
+import {
+  registerDevice,
+  pullCountryList,
+  pullCityList,
+  pullServerList,
+  pullServerConf,
+  getIp,
+  showNotification,
+  getVpnMetric
+} from '../system/ipcs.js'
+import { dnsList } from '../system/dns/dnsList.js'
+import { adapterSpeed } from '../system/stats/adapter.js'
+
+
+export function registerIpcHandlers(ipcMain) {
+
+  ipcMain.handle('triggerConnection', async (event, serverParms) => {
+    console.log('vpn connection trigger on main process')
+    return await vpnConnet(serverParms);
+  })
+
+  ipcMain.handle('triggerDisconnection', async (event) => {
+    console.log('vpn disconnection trigger on main process')
+    return await vpnDisconnect()
+  })
+
+  ipcMain.handle('vpnConnStatus', (event, serverObj) => {
+    return global.vpnConnStatus
+  })
+
+  ipcMain.handle('registerDevice', async () => {
+    console.log('device registration triggered on main process')
+    return registerDevice()
+  })
+
+  ipcMain.handle('getCountries', async (event, args) => {
+    return pullCountryList(args)
+  })
+
+  ipcMain.handle('getCities', async (event, device_token, countryCode) => {
+    return pullCityList(device_token, countryCode)
+  })
+
+  ipcMain.handle('getServers', async (event, device_token, countryCode, cityCode) => {
+    return pullServerList(device_token, countryCode, cityCode)
+  })
+
+  ipcMain.handle('getServerConf', async (event, device_token, countryCode, cityCode, serverId) => {
+    return pullServerConf(device_token, countryCode, cityCode, serverId)
+  })
+
+  ipcMain.handle('sysOpen', async (event, ...url) => {
+    shell.openExternal(url[0], { activate: true });
+  })
+
+  ipcMain.handle('getIp', async (event, device_token) => {
+    return getIp(device_token);
+  })
+
+  ipcMain.handle('appVersion', async (event) => {
+    let appVersion = app.getVersion();
+    return appVersion;
+  })
+
+  ipcMain.handle('openMailClient', async (event, mailObj) => {
+    return new Promise((resolve, reject) => {
+      const mailtoLink = `mailto:${mailObj.to}?subject=${mailObj.subject}&body=${mailObj.body}`;
+      shell.openExternal(mailtoLink, { activate: true }, (error) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
+      });
+    });
+  })
+
+  ipcMain.handle('pushNotification', async (event, title, body) => {
+    if (title && body) {
+      showNotification(title, body);
+    }
+
+  })
+
+  ipcMain.handle('vpnMetric', async (event) => {
+    return getVpnMetric();
+  })
+
+  ipcMain.handle('getDnsList', async (event) => {
+
+    return {
+      dnsList: dnsList,
+      selectedDns: vpnObj.dnsIndex
+    };
+
+  })
+  ipcMain.handle('setDns', async (event, dnsId) => { vpnObj.dnsIndex = dnsId || 0; })
+
+  ipcMain.handle('adapterSpeed', async (event) => { return adapterSpeed(global.adapterName); });
+
+}
