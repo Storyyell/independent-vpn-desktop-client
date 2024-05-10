b3c880cac083ce9fadfdb5dcd74839ebd7a489d2
anandanmb01 <anandanmb01@gmail.com>
2024-05-10 13:56:05 +0530

    app background added

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -74,7 +74,7 @@ function App() {
                     <DnsListProvider>
                       <ThemeProvider theme={theme}>
                         <CssBaseline />
-                        <Box className="app grad">
+                        <Box className="app svg-background">
                           <Header />
                           <Home />
                         </Box>
