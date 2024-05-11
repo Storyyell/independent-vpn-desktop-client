981aee7e1f920f54bcad84096550152cc5f1089d
anandanmb01 <anandanmb01@gmail.com>
2024-05-11 10:52:49 +0530

    country list fetcg added

--- src/renderer/src/components/GeoSelection/CountryList.jsx
+++ src/renderer/src/components/GeoSelection/CountryList.jsx
@@ -1,17 +1,44 @@
 import React from 'react'
 import { GeoItem } from '../GeoItem/GeoItem'
 import { Typography } from '@mui/material'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { deviceTokenState } from '../../atoms/app/token'
+import { countryListState } from '../../atoms/available/countryList'
 
+const dataValidityPeroid = 10 * 60 * 1000 // 10minutes
 
 
 const CountryList = (props) => {
-  let serverList = props.serverList
-  let countryListProcessed = props.countryListProcessed
-  let favList = props.favList
-  let setFavList = props.setFavList
   let setLoadCityList = props.setLoadCityList
   let setSelectedItems = props.setSelectedItems
   let setProcessListUpdate = props.setProcessListUpdate
+
+
+  const deviceToken = useRecoilValue(deviceTokenState);
+  const [countryList, setCountryList] = useRecoilState(countryListState);
+
+  const now = new Date();
+
+  if (deviceToken && (now - countryList.timeStamp > dataValidityPeroid)) {
+
+    console.log("county list fetch trigerred")
+
+    window.api.getCountries(deviceToken)
+      .then((res) => {
+        if (res.data) {
+          setCountryList({
+            timeStamp: new Date(),
+            data: res.data
+          })
+        }
+      })
+      .catch((e) => {
+        console.log(e)
+      })
+
+  }
+
+  let countryListProcessed = countryList.data
 
   const handleCountryChange = (countryId_) => {
     setProcessListUpdate(d => !d)
@@ -25,10 +52,8 @@ const CountryList = (props) => {
   return (
     <>
       {
-        serverList?.countries?.data
-
+        countryListProcessed.length > 0
           ?
-
           countryListProcessed?.map((d, i) => {
             return (
               <GeoItem key={d?.id} data={d} geoType='country' onClick={(val) => {
@@ -42,6 +67,7 @@ const CountryList = (props) => {
 
           <Typography sx={{ m: 2 }}>
             Loading country list ...
+
           </Typography>
       }
     </>
