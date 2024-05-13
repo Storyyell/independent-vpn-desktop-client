a5fb34963603a339fac3b289f99dfca78fcaffc9
anandanmb01 <anandanmb01@gmail.com>
2024-05-13 22:05:57 +0530

    random geocoordinate on each load

--- src/renderer/src/atoms/app/geoCordinate.js
+++ src/renderer/src/atoms/app/geoCordinate.js
@@ -1,12 +1,18 @@
 import { atom } from "recoil";
 
+// Function to generate a random latitude between -90 and 90 degrees
+const randomLatitude = () => Math.random() * 180 - 90;
+
+// Function to generate a random longitude between -180 and 180 degrees
+const randomLongitude = () => Math.random() * 360 - 180;
+
 const geoCoordinateState = atom({
   key: 'geoCoordinate',
   default: {
-    lat: 50.8101,
-    lng: 4.3247,
+    lat: randomLatitude(),
+    lng: randomLongitude(),
     ip: ""
   },
 });
 
-export { geoCoordinateState }
+export { geoCoordinateState };
