f731532a36f4c8815163f88995c8f27e5493f382
anandanmb01 <anandanmb01@gmail.com>
2024-05-19 13:15:59 +0530

    code commented

--- src/main/index.js
+++ src/main/index.js
@@ -30,7 +30,6 @@ console.log = log.log;
 console.log(`log path :=> ${log.transports.file.getFile().path}`);
 
 
-
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
@@ -55,13 +54,9 @@ app.whenReady().then(() => {
 
     // Default open or close DevTools by F12 in development
     // and ignore CommandOrControl + R in production.
-    // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
     app.on('browser-window-created', (_, window) => {
       optimizer.watchWindowShortcuts(window)
     })
-
-    // IPC test
-    ipcMain.on('ping', () => console.log('pong'))
 
     createWindow()
 
