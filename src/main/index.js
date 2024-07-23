07e4a549272dffd500ab48ccbdbf0a103ccff1e3
anandanmb01 <anandanmb01@gmail.com>
2024-07-24 00:34:58 +0530

    app model id updated 4 windows

--- src/main/index.js
+++ src/main/index.js
@@ -36,7 +36,7 @@ app.whenReady().then(() => {
     })
 
     // Set app user model id for windows
-    electronApp.setAppUserModelId('com.electron')
+    electronApp.setAppUserModelId('co.sentinel')
 
     // Default open or close DevTools by F12 in development
     // and ignore CommandOrControl + R in production.
