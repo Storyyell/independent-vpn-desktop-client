164a6b4855011f34831e7a572e205ca02bcabdaa
anandanmb01 <anandanmb01@gmail.com>
2024-07-24 23:16:07 +0530

    removed test

--- test/networktest.js
+++
@@ -1,9 +0,0 @@
-
-import Network from '../src/main/system/classes/network.js';
-
-const netObj = new Network()
-
-// console.log(await netObj.getGatewayAdapterIp())
-// console.log(await netObj.getGatewayInterfaceName())
-// console.log(await netObj.getGatewayAdapterIps())
-// console.log(await netObj.getIPv4FromDomain('facebook.com'))
